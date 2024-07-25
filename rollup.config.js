98b42903ade0e5f1e6735a73885b7e6974e28594
LeminGhori <ghorilemin@gmail.com>
2024-07-25 21:33:50 +0530

    add

--- rollup.config.js
+++ rollup.config.js
@@ -1,30 +1,35 @@
-import {terser} from 'rollup-plugin-terser';
+import babel from '@rollup/plugin-babel';
+import { terser } from 'rollup-plugin-terser';
 
 const devMode = (process.env.NODE_ENV === 'development');
-console.log(`${ devMode ? 'development' : 'production' } mode bundle`);
-
+console.log(`${devMode ? 'development' : 'production'} mode bundle`);
 
 export default [
   {
     input: 'src/index.js',
     output: {
-      file: "dist/index.js",
+      file: 'dist/index.js',
       format: 'es',
-      sourcemap: devMode ? 'inline' : false,
-      plugins: [
-        terser({
-          ecma: 2020,
-          mangle: { toplevel: true },
-          compress: {
-            module: true,
-            toplevel: true,
-            unsafe_arrows: true,
-            drop_console: !devMode,
-            drop_debugger: !devMode
-          },
-          output: { quote_style: 1 }
-        })
-      ]
-    }
+      sourcemap: devMode ? 'inline' : false
+    },
+    plugins: [
+      babel({
+        babelHelpers: 'bundled',
+        exclude: 'node_modules/**',
+        presets: ['@babel/preset-env']
+      }),
+      terser({
+        ecma: 2020,
+        mangle: { toplevel: true },
+        compress: {
+          module: true,
+          toplevel: true,
+          unsafe_arrows: true,
+          drop_console: !devMode,
+          drop_debugger: !devMode
+        },
+        output: { quote_style: 1 }
+      })
+    ]
   }
-]
+];
